4cdfcda3dc07dca6de4bea9a3b8f73f1dfa3b12c
icharlem <charles.meruwoma@gmail.com>
2024-01-30 16:53:24 -0600

    Initial Commit

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,16 @@
 import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'icharlem',
-  authorAddress: 'charles.meruwoma@gmail.com',
+  author: 'cmstack',
+  authorAddress: 'charles@cmstack.xyz',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.0.0',
   name: 'cmstack-base-lib',
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/charles.meruwoma/cmstack-base-lib.git',
+  repositoryUrl: 'https://github.com/cmstack/cmstack-base-lib.git',
+  githubOptions: {
+    mergify: false,
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
